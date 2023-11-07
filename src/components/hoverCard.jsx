56cedfee5e335840ac95e10bf007274421610115
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2023-11-07 15:02:47 +0300

    update

---
+++ src/components/hoverCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    Popover,
+    PopoverHandler,
+    PopoverContent,
+    Button,
+    Avatar,
+    Typography,
+  } from "@material-tailwind/react";
+
+const hoverCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default hoverCard
