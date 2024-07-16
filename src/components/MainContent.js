fc252e2a1b7bf3e4247a0907fe301950ed6590df
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-07-16 19:39:11 +0300

    update

---
+++ src/components/MainContent.js
@@ -0,0 +1,25 @@
+// MainContent.js
+import React from "react";
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Portfolio from "./Portfolio";
+import SocialLinks from "./SocialLinks";
+
+function MainContent() {
+  return (
+    <div className="">
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default MainContent;
