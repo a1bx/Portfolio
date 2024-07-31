7dac541646ffbd2ab347ff5ad923f58cd1b4716b
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-07-31 16:13:23 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,12 +15,12 @@ const Home = () => {
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have experience building and designing software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            Currently, I love to work on web applications using technologies like
+            React, Tailwind, Next JS, and GraphQL.
           </p>
           <div>
             <Link
-              to="/Portfolio.jsx"
+              to="portfolio"
               smooth
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
