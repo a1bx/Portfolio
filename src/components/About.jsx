8f42a811ba8461e7348e16f92b06e59675a24b38
Jeremiah Nyok <98920854+a1bx@users.noreply.github.com>
2025-03-18 16:55:33 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,23 +9,23 @@ const About = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
+            About Me
           </p>
         </div>
         <p className="text-xl mt-10">
-        Hello! I'm a dedicated software engineer and computer science student with a passion for coding and problem-solving. 
-        Beyond my tech skills, I have a unique passion for animation, which I've cultivated alongside my technical pursuits. 
-        This diverse combination of skills and interests makes me a well-rounded individual who brings creativity,
-        dedication, and a holistic approach to both the tech world and animation world.
+        Hello! I am a dedicated software engineer and a computer science graduate with a passion for coding and problem-solving.
+        I have a solid foundation in mobile development using Dart, Kotlin and Flutter sdk, enabling me to create cross-platform applications.
+        My expertise extends to web development using TypeScript, JavaScript and also it's popular libraries and frameworks such as React and Next.Js
+        enabling me to create dynamic and interactive web applications.
         </p>
 
         <br />
 
-        <p className="text-xl">
+{/*         <p className="text-xl">
         I have a solid foundation in JavaScript,Typescript enabling me to create dynamic and interactive web applications.
         I'm also experienced with popular JavaScript libraries and frameworks such as React, Vite, Next.Js and Vue.js, allowing me to build efficient and user-friendly front-end interfaces.
         My expertise extends to server-side development using Node.js, enabling me to create full-stack applications.
-        </p>
+        </p> */}
       </div>
     </div>
   );
